ab12732e9d98a721a86aca9852b481581f2562c5
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-09 14:11:02 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -5,58 +5,64 @@ public class Main {
 
     static int N, M;
     static int[][] map;
-    static boolean[][] visited;
+    static int[][] distance;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
+    static class Node{
+        int x;
+        int y;
+        Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N+1][M+1];
-        visited = new boolean[N+1][M+1];
 
-        for (int i = 0; i < N; i++) {
+        map = new int[N][M];
+        distance = new int[N][M];
+        for(int i=0; i<N; i++) Arrays.fill(distance[i], -1);
+
+        for(int i=0; i<N; i++){
             String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j) - '0';
+            for(int j=0; j<M; j++){
+                map[i][j] = line.charAt(j)- '0';
             }
         }
-        System.out.println(bfs(0,0));
-
+        bfs(0, 0);
+        System.out.println(distance[N-1][M-1]);
     }
 
-    public static int bfs(int x, int y){
+    public static void bfs(int x, int y){
 
-        visited[x][y] = true;
-        Deque<int []> dq = new ArrayDeque<>();
-        dq.addLast(new int[]{x,y});
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(x, y));
+        distance[x][y] = 1;
 
-        while(!dq.isEmpty()) {
-            int[] now = dq.pollFirst();
-            int nowX = now[0];
-            int nowY = now[1];
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            int curX = cur.x;
+            int curY = cur.y;
 
-            if(nowX == N - 1 && nowY == M - 1){
-                return map[nowX][nowY];
-            }
+            for(int i=0; i<4; i++){
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
+                // 범위를 나가면 continue
+                if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+                // 이미 방문했으면 continue
+                if(distance[nextX][nextY] != -1) continue;
+                // 0이라서 못 가는 곳이면 continue
+                if(map[nextX][nextY] == 0) continue;
 
-                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
-                if (visited[nextX][nextY]) continue;
-                if (map[nextX][nextY] == 0) continue;
-
-                visited[nextX][nextY] = true;
-                map[nextX][nextY] = map[nowX][nowY] + 1;
-                dq.addLast(new int[]{nextX, nextY});
+                distance[nextX][nextY] = distance[curX][curY] + 1;
+                q.add(new Node(nextX,nextY));
             }
         }
-        return -1;
-
     }
 
 }
