6882edf43687bbcb644c160781fd36acfc51ef52
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-20 14:35:07 +0900

    [Silver II] Title: 촌수계산, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -5,15 +5,8 @@ public class Main {
 
     static int N, M;
     static int target1, target2;
-    static boolean[][] map;
-    static int[][] dist;
-    static class Node{
-        int x, y;
-        Node(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static List<Integer>[] map;
+    static int[] dist;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,44 +15,39 @@ public class Main {
         target1 = Integer.parseInt(st.nextToken());
         target2 = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(br.readLine());
-        map = new boolean[N+1][N+1];
-        dist = new int[N+1][N+1];
+
+        map = new ArrayList[N+1];
+        for(int i=1; i<=N; i++) map[i] = new ArrayList<>();
+
+        dist = new int[N+1];
+        Arrays.fill(dist, -1);
 
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int parent = Integer.parseInt(st.nextToken());
             int child = Integer.parseInt(st.nextToken());
-            map[parent][child] = true;
-            map[child][parent] = true;
+            map[parent].add(child);
+            map[child].add(parent);
         }
+
         System.out.println(bfs());
+
 
     }
 
     public static int bfs(){
-        Queue<Node> q = new ArrayDeque<>();
-        for(int i=1; i<=N; i++){
-            if(map[target1][i]){
-                dist[target1][i] = 1;
-                dist[i][target1] = 1;
-                q.offer(new Node(target1, i));
-            }
-        }
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(target1);
+        dist[target1] = 0;
 
         while(!q.isEmpty()){
-            Node cur = q.poll();
-            int curX = cur.x;
-            int curY = cur.y;
+            int cur = q.poll();
+            if(cur == target2) return dist[cur];
 
-            if(curY == target2) return dist[curX][curY];
-
-            for(int i=1; i<=N; i++){
-                if(!map[curY][i]) continue;
-                else if(dist[curY][i] == 0){
-                    dist[curY][i] = dist[curX][curY] + 1;
-                    dist[i][curY] = dist[curY][i];
-                    q.offer(new Node(curY, i));
-                }
+            for(int next : map[cur]){
+                if(dist[next] != -1) continue;
+                dist[next] = dist[cur]+1;
+                q.offer(next);
             }
         }
 
