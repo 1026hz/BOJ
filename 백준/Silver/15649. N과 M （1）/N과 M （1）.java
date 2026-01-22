e15450c902ba02e1a03a1ae8f8622a358960c54c
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-22 11:01:28 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 22728 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+    static boolean[] isUsed;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M+1];
+        isUsed = new boolean[N+1];
+        solve(1);
+        System.out.println(sb);
+    }
+    static void solve(int now){
+        if(M+1 == now){
+            for(int i=1; i<=M; i++){
+                sb.append(arr[i]).append(' ');
+            }sb.append('\n');
+            return;
+        }
+        for(int i=1; i<=N; i++){
+            if(!isUsed[i]){
+                isUsed[i] = true;
+                arr[now] = i;
+                solve(now + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+
+}
