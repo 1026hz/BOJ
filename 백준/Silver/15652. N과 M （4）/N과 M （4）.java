b28c9acc7fd8f10cbcd1624428bedfad7c945e6f
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-23 16:33:55 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 17412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] isUsed;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M+1];
+        isUsed = new boolean[N+1];
+        solve(1, 1);
+        System.out.println(sb);
+
+    }
+    static void solve(int nowIdx, int nowNum){
+
+        isUsed[nowNum] = false;
+
+        if (nowIdx == M+1){
+            for(int i=1; i<=M; i++){
+                sb.append(arr[i]).append(' ');
+            }sb.append('\n');
+            return;
+        }
+
+        for(int i=1; i<=N; i++){
+            if(!isUsed[i] && arr[nowIdx - 1] <= i) {
+                arr[nowIdx] = i;
+                isUsed[i] = true;
+                solve(nowIdx+1, i);
+                isUsed[i] = false;
+            }
+        }
+
+    }
+
+}
