6b6049a38391f3c4730632ac4fbb1ca185d4a4ed
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-27 00:03:58 +0900

    [Silver II] Title: 연속합, Time: 220 ms, Memory: 23768 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] numbers = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[N];
+        dp[0] = numbers[0];
+        int max = numbers[0];
+
+        for(int i=1; i<N; i++) {
+            dp[i] = Math.max(dp[i-1] + numbers[i], numbers[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+
+    }
+
+}
