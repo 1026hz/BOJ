c6b5d5619d400f902e4d17def7a115ef3bcacd9e
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-27 10:24:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 96 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -2,27 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n1 = Integer.parseInt(st.nextToken());
-        int n2 = Integer.parseInt(st.nextToken());
-
-        int tmp = 0;
-        int a = n1;
-        int b = n2;
-        while(b > 0){
-            tmp = a % b;
-            a = b;
-            b = tmp;
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int x = N;
+        int y = M;
+        while(y != 0){
+            int tmp = x % y;
+            x = y;
+            y = tmp;
         }
-        int gcd = a;
-        int lcm = n1*n2/gcd;
 
-        System.out.println(gcd);
-        System.out.println(lcm);
-
-
+        System.out.println(x);
+        System.out.println(x*(N/x)*(M/x));
 
     }
+
 }
