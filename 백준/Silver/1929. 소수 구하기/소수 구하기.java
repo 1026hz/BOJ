70f4ca0bcc3cd20a2731e9ab891145a23c3ec198
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-27 10:38:12 +0900

    [Silver III] Title: 소수 구하기, Time: 200 ms, Memory: 20664 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,28 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        boolean[] numbers = new boolean[N+1];
-        numbers[0] = true;
-        numbers[1] = true;
-
-
-        for(int i=2; i*i<=N; i++){
-            if(!numbers[i]) {
-                for (int j = i*i; j <= N; j += i) {
-                    numbers[j] = true;
+        boolean[] isPrime = new boolean[N+1];
+        Arrays.fill(isPrime, true);
+        isPrime[1] = false;
+        if(N>=2) {
+            for (int i = 2; i * i <= N; i++) {
+                if (!isPrime[i])
+                    continue;
+                for (int j = i + i; j <= N; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
         StringBuilder sb = new StringBuilder();
         for(int i=M; i<=N; i++){
-            if(!numbers[i]) sb.append(i).append('\n');
+            if(isPrime[i]) sb.append(i).append('\n');
         }
-        System.out.println(sb);
 
+        System.out.println(sb);
     }
+
 }
