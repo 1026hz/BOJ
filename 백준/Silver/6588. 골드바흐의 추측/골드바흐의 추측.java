2b59774e43a80764f53155e64a7bd6909a43e2fc
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-01-27 10:57:25 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 504 ms, Memory: 38436 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -2,40 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        boolean[] notPrime = new boolean[1000001];
-        notPrime[2] = false;
-
-        for(int i=2; i*i<=1000000; i++){
-            if (!notPrime[i]) {
-                for (int j = i * i; j <= 1000000; j += i) {
-                    notPrime[j] = true;
-                }
+        boolean[] isPrime = new boolean[1000001];
+        Arrays.fill(isPrime, true);
+        isPrime[1] = false;
+        for(int i=1; i*i<=1000000; i++){
+            if(!isPrime[i]) continue;
+            for(int j=i*i; j<=1000000; j+=i){
+                isPrime[j] = false;
             }
         }
 
-        int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        while (n != 0){
+        while(true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0){
+                System.out.println(sb);
+                return;
+            }
 
-            int a = 0, b = 0;
-            boolean flag = false;
-
-            for(int i=2; i<=n/2; i++){
-                if (!notPrime[i] && !notPrime[n-i]){
-                    a = i;
-                    b = n-i;
-                    flag = true;
+            for(int i=3; i<=n; i++){
+                if(isPrime[i] && isPrime[n-i]) {
+                    sb.append(n).append(" = ").append(i).append(" + ").append(n-i).append('\n');
                     break;
                 }
+                if(i==n) sb.append("Goldbach's conjecture is wrong.");
             }
-            if (!flag) sb.append("Goldbach's conjecture is wrong.").append('\n');
-            else sb.append(n + " = " + a + " + " + b).append('\n');
 
-            n = Integer.parseInt(br.readLine());
         }
-        System.out.println(sb);
+
     }
 }
