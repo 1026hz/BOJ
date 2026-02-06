794c22e56583ebd7c1606f5bc3d51a0f453b5245
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-02-06 11:18:45 +0900

    [Bronze II] Title: 다이얼, Time: 92 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] sec = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+        int time = 0;
+        for(char c : s.toCharArray()){
+            time += sec[c-'A'];
+        }
+        System.out.println(time);
+
+    }
+}
