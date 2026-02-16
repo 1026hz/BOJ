b69890a31faee1a765846ecd1c4818e417dfc7af
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-02-16 13:09:07 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,23 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+6];
+        int[] dp = new int[N+1];
         dp[3] = 1;
-        dp[5] = 1;
+        if(N >= 5) dp[5] = 1;
 
-        for(int i=3; i<=n; i++){
-            if (dp[i] != 0) {
-                if (dp[i+3] == 0) dp[i+3] = dp[i]+1;
-                if (dp[i+5] == 0) dp[i+5] = dp[i]+1;
-            }
+
+        for(int i=5; i<=N; i++){
+            if(dp[i-3] != 0 && dp[i-5] != 0) dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+            else if(dp[i-3] != 0 && dp[i-5] == 0) dp[i] = dp[i-3] + 1;
+            else if(dp[i-3] == 0 && dp[i-5] != 0) dp[i] = dp[i-5] + 1;
         }
-        if (dp[n] == 0) System.out.println(-1);
-        else System.out.println(dp[n]);
 
+        if(dp[N] == 0) System.out.println(-1);
+        else System.out.println(dp[N]);
     }
 }
-
