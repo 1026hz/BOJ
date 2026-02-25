9d8d239fdb103c7b1e9c8b9aa3e91511e0e9f596
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-02-26 01:33:04 +0900

    [Silver I] Title: IOIOI, Time: 340 ms, Memory: 113800 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -9,27 +9,15 @@ public class Main {
         int M = Integer.parseInt(br.readLine());
         String S = br.readLine();
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("I");
-        for(int i=0; i<N; i++) sb.append("OI");
-        String P = sb.toString();
-
-        int idx = 0;
         int count = 0;
-        for(int i=0; i<S.length(); i++){
-            if(S.charAt(i) == P.charAt(idx)){
-                //System.out.println("same : " + i + " " + P.charAt(idx));
-                idx++;
-                if(idx == P.length()){
-                    count++;
-                    idx = 0;
-                    i-=P.length()-2;
-                }
-            } else{
-                idx = 0;
-                if(S.charAt(i) == P.charAt(idx)){
-                    idx++;
-                }
+        int k = 0;
+        for(int i=1; i<M-1; i++){
+            //System.out.println(i + " : " + S.substring(i-1, i+2));
+            if(S.substring(i-1, i+2).equals("IOI")) k++;
+            else if(S.substring(i-1,i + 2).equals("OIO")) continue;
+            else k = 0;
+            if(k >= N){
+                count++;
             }
         }
 
