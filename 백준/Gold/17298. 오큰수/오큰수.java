29c86bdb34f3298f455c0140d840d8ac2be81c5e
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-03-06 20:18:48 +0900

    [Gold IV] Title: 오큰수, Time: 768 ms, Memory: 151912 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,31 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] num = new int[N+1];
-        int[] ans = new int[N+1];
-        Arrays.fill(ans, -1);
-        Deque<Integer> stack = new ArrayDeque<>();
+        int[] numbers = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=1; i<=N; i++){
-            int curNum = Integer.parseInt(st.nextToken());
-            num[i] = curNum;
-            while(!stack.isEmpty() && num[stack.peekLast()] < curNum){
-                ans[stack.peekLast()] = curNum;
-                stack.pollLast();
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] ans = new int[N+1];
+        Arrays.fill(ans, -1);
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=1; i<=N; i++){
+            while(!stack.isEmpty() && numbers[stack.peekLast()] < numbers[i]){
+                ans[stack.pollLast()] = numbers[i];
             }
-            stack.addLast(i);
+            stack.offerLast(i);
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=N; i++){
-            sb.append(ans[i]).append(" ");
-        }
-
+        for(int i=1; i<=N; i++) sb.append(ans[i]).append(' ');
         System.out.println(sb);
     }
 }
