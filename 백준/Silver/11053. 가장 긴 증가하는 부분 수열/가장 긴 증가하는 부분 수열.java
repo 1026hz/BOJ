cfb76d6b4b679544fadc6cf97947cc01851202e5
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-03-09 14:29:05 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 108 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,32 +7,16 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int[] A = new int[N];
         int[] tails = new int[N];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) A[i] = Integer.parseInt(st.nextToken());
 
         int size = 0;
         for(int num : A){
-            int position = lowerBound(tails, size, num);
+            int position = Arrays.binarySearch(tails, 0, size, num);
+            if(position < 0) position = -(position + 1);
             tails[position] = num;
             if(position == size) size++;
         }
-
         System.out.println(size);
     }
-
-    static int lowerBound(int[] arr, int size, int target){
-        int left = 0;
-        int right = size;
-
-        while(left < right){
-            int mid = (left + right) / 2;
-            if (arr[mid] >= target) right = mid;
-            else left = mid + 1;
-        }
-
-        return left;
-    }
-
-
 }
