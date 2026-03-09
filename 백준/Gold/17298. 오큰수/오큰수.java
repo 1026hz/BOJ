5d2bc2217b11b15836da43a6386a62114e7507f1
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-03-09 13:27:02 +0900

    [Gold IV] Title: 오큰수, Time: 840 ms, Memory: 152660 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -4,28 +4,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        int[] numbers = new int[N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=N; i++){
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int[] ans = new int[N+1];
+        int[] A = new int[N];
+        int[] ans = new int[N];
         Arrays.fill(ans, -1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) A[i] = Integer.parseInt(st.nextToken());
 
         Deque<Integer> stack = new ArrayDeque<>();
-
-        for(int i=1; i<=N; i++){
-            while(!stack.isEmpty() && numbers[stack.peekLast()] < numbers[i]){
-                ans[stack.pollLast()] = numbers[i];
+        for(int i=0; i<N; i++){
+            while(!stack.isEmpty() && A[stack.peekLast()] < A[i]){
+                ans[stack.peekLast()] = A[i];
+                stack.pollLast();
             }
-            stack.offerLast(i);
+            stack.offer(i);
         }
 
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=N; i++) sb.append(ans[i]).append(' ');
+        for(int i=0; i<N; i++) sb.append(ans[i]).append(' ');
+
         System.out.println(sb);
+
     }
 }
