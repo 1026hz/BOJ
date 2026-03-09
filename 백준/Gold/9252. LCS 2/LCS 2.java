4a7a994b434dc98bd98d05a32ad6725cee3552a3
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-03-09 15:49:01 +0900

    [Gold III] Title: LCS 2, Time: 144 ms, Memory: 19056 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -4,54 +4,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s1 = br.readLine();
-        String s2 = br.readLine();
-        int N = s1.length();
-        int M = s2.length();
-        int[][] dp = new int[N + 1][M + 1];
-        int[][][] parent = new int[N+1][M+1][2];
+        String S1 = br.readLine();
+        String S2 = br.readLine();
+        int[][] dp = new int[S1.length()+1][S2.length()+1];
 
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M; j++){
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    parent[i][j][0] = i-1;
-                    parent[i][j][1] = j-1;
-                } else{
-                    if(dp[i-1][j] >= dp[i][j-1]){
-                        dp[i][j] = dp[i-1][j];
-                        parent[i][j][0] = i-1;
-                        parent[i][j][1] = j;
-                    }
-                    else{
-                        dp[i][j] = dp[i][j-1];
-                        parent[i][j][0] = i;
-                        parent[i][j][1] = j-1;
-                    }
-                }
+        for(int i=1; i<=S1.length(); i++){
+            for(int j=1; j<=S2.length(); j++){
+                if(S1.charAt(i-1) == S2.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
-        int size = dp[N][M];
+
+        int idx1 = S1.length();
+        int idx2 = S2.length();
         List<Character> ans = new ArrayList<>();
-        int curX = N;
-        int curY = M;
 
-        while(curX > 0 && curY > 0){
-            int nextX = parent[curX][curY][0];
-            int nextY = parent[curX][curY][1];
-
-            if(curX -1 == nextX && curY - 1 == nextY) ans.add(s1.charAt(curX-1));
-
-            curX = nextX;
-            curY = nextY;
+        while(idx1 > 0 && idx2 > 0){
+            if(S1.charAt(idx1-1) == S2.charAt(idx2-1)){
+                ans.add(S1.charAt(idx1-1));
+                idx1 -=1;
+                idx2 -=1;
+            } else{
+                if(dp[idx1-1][idx2] >= dp[idx1][idx2-1]) idx1 -=1;
+                else idx2 -=1;
+            }
         }
 
-        Collections.reverse(ans);
-
         StringBuilder sb = new StringBuilder();
-        sb.append(size).append('\n');
-        for(char c : ans) sb.append(c);
-
+        sb.append(ans.size()).append('\n');
+        for(int i=ans.size()-1; i>=0; i--) sb.append(ans.get(i));
         System.out.println(sb);
 
     }
