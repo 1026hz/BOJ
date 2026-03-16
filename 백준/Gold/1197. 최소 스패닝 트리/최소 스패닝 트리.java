0e1d9849b9a178e4479a3b4abdb48a7b3fd833b5
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-03-16 09:46:07 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 516 ms, Memory: 48476 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,28 +3,23 @@ import java.util.*;
 
 public class Main {
 
-    static int[] parent;
-    static int[] rank;
-
-    static class Edge implements Comparable<Edge>{
+    static class Edge{
         int a, b, w;
-
         Edge(int a, int b, int w){
             this.a = a;
             this.b = b;
             this.w = w;
         }
-
-        @Override
-        public int compareTo(Edge o){
-            return Integer.compare(this.w, o.w);
-        }
     }
-    public static void main(String[] args) throws IOException {
+    static int V, E;
+    static int[] rank;
+    static int[] parent;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
 
         Edge[] edges = new Edge[E];
 
@@ -32,43 +27,35 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(a, b, w);
+            int c = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(a, b, c);
         }
 
+        Arrays.sort(edges,(a,b) -> Integer.compare(a.w, b.w));
         makeSet(V);
-        Arrays.sort(edges);
 
-        int total = 0;
         int picked = 0;
-
+        int weight = 0;
         for(Edge e : edges){
             int a = e.a;
             int b = e.b;
             if(union(a,b)){
-                total += e.w;
+                weight += e.w;
                 picked++;
-
-                if(picked == V-1) break;
             }
+            if(picked == V - 1) break;
         }
+        System.out.println(weight);
 
-        System.out.println(total);
     }
 
-    static void makeSet(int n){
-        parent = new int[n+1];
-        rank = new int[n+1];
-        for(int i=1; i<=n; i++){
-            parent[i] = i;
+    static void makeSet(int N){
+        rank = new int[N+1];
+        parent = new int[N+1];
+        for(int i=0; i<N; i++){
             rank[i] = 0;
+            parent[i] = i;
         }
-    }
-
-    static int find(int n){
-        if(parent[n] == n) return n;
-        parent[n] = find(parent[n]);
-        return parent[n];
     }
 
     static boolean union(int a, int b){
@@ -76,14 +63,20 @@ public class Main {
         b = find(b);
 
         if(a == b) return false;
-        if(rank[a] > rank[b]) parent[b] = a;
-        else if(rank[b] > rank[a]) parent[a] = b;
-        else{
-            rank[a]++;
+        if(a >= b) {
             parent[b] = a;
+            rank[a]++;
+        } else {
+            parent[a] = b;
+            rank[b]++;
         }
-
         return true;
+    }
+
+    static int find(int n){
+        if(parent[n] == n) return n;
+        parent[n] = find(parent[n]);
+        return parent[n];
     }
 
 }
