6473fad82ba9a9ce94ea9ac0ffef45edccb6f555
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-06-04 02:22:37 +0900

    [level 2] Title: 소수 찾기, Time: 9.53 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+
+    Set<Integer> set = new HashSet<>();
+
+    public int solution(String numbers) {
+
+        boolean[] visited = new boolean[numbers.length()];
+        
+        dfs("", numbers, visited);
+
+        int answer = 0;
+
+        for (int num : set) {
+            if (isPrime(num)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    private void dfs(String current, String numbers, boolean[] visited) {
+
+        if (!current.isEmpty()) {
+            set.add(Integer.parseInt(current));
+        }
+
+        for (int i = 0; i < numbers.length(); i++) {
+
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs(current + numbers.charAt(i), numbers, visited);
+            visited[i] = false;
+        }
+    }
+
+    private boolean isPrime(int n) {
+
+        if (n < 2) return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
