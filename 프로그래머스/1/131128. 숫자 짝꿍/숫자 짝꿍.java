4d8c108feca70b64e5f6980a5fffbed5166c8889
HeeJae <132039586+1026hz@users.noreply.github.com>
2026-07-02 00:19:14 +0900

    [level 1] Title: 숫자 짝꿍, Time: 60.67 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    public String solution(String X, String Y) {
+        
+        int[] cntX = new int[10];
+        int[] cntY = new int[10];
+
+        for(char c : X.toCharArray()){
+            cntX[c-'0']++;
+        }
+
+        for(char c : Y.toCharArray()){
+            cntY[c-'0']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=9;i>=0;i--){
+            int cnt = Math.min(cntX[i], cntY[i]);
+
+            while(cnt-- >0){
+                sb.append(i);
+            }
+        }
+
+        if(sb.length()==0)
+            return "-1";
+
+        if(sb.charAt(0)=='0')
+            return "0";
+
+        return sb.toString();
+    }
+}
